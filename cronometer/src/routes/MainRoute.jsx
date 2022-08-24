46fd28b126349113de1c1a7d2018ec6e085b7e6d
Aakash Kushwaha <99542364+Aakash-kushwaha@users.noreply.github.com>
2022-08-24 22:30:05 +0530

    progress bar lib

--- cronometer/src/routes/MainRoute.jsx
+++ cronometer/src/routes/MainRoute.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Routes,Route} from 'react-router-dom';
 import { About } from '../pages/About/About';
+import Energysummaryprogressbar from '../pages/Diarypage/Diary/Energysummaryprogressbar';
 import Homepage from '../pages/homepage/Homepage';
 
 
@@ -8,6 +9,8 @@ import Homepage from '../pages/homepage/Homepage';
 const MainRoute = () => {
   return (
     <div>
+
+
       <Routes>
         <Route path='/about' element={<About/>}  />
         <Route path='/' element={<Homepage/>}  />
