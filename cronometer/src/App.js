9f6b8294c72293e11e115cbd9aaa7eb4f7fcd1f6
Shreeprada <shreepradadb@gmail.com>
2022-08-26 16:21:21 +0530

    header

--- cronometer/src/App.js
+++ cronometer/src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 import MainRoute from './routes/MainRoute';
 import {Provider} from "react-redux"
 import {store} from "./redux/Store"
-
-
+import LoginHeader from './components/header/LoginHeader';
 function App() {
   return (
     <Provider store={store}>
     <div className="App">
+    <LoginHeader/>
     <MainRoute/>
     </div>
 </Provider>);
