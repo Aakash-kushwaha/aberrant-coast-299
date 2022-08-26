0d2c871546ac754353343da9750751d186f04d63
Mohit Patil <97459016+mohit7067@users.noreply.github.com>
2022-08-26 23:18:21 +0530

    todays work done

--- cronometer/src/routes/MainRoute.jsx
+++ cronometer/src/routes/MainRoute.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
-import {Routes,Route} from 'react-router-dom';
-import { About } from '../pages/About/About';
-import Addfood from '../pages/Diarypage/Diary/Addfood/Addfood';
-import Energysummaryprogressbar from '../pages/Diarypage/Diary/Energysummaryprogressbar';
-import Highlighted_container from '../pages/Diarypage/Diary/Highlighted_container';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { About } from "../pages/About/About";
+import Addfood from "../pages/Diarypage/Diary/Addfood/Addfood";
+import Energysummaryprogressbar from "../pages/Diarypage/Diary/Energysummaryprogressbar";
+import Highlighted_container from "../pages/Diarypage/Diary/Highlighted_container";
 
-import Homepage from '../pages/homepage/Homepage';
-import Plans from '../pages/plans/Plans';
-import Settings from '../pages/settings/Settings';
+import Homepage from "../pages/homepage/Homepage";
+import Plans from "../pages/plans/Plans";
+import Settings from "../pages/settings/Settings";
+import Signup from "../pages/signup/Signup";
 
 const MainRoute = () => {
   return (
     <div>
       <Routes>
-        <Route path='/about' element={<About/>}  />
-        <Route path='/' element={<Homepage/>}  />
-        <Route path="/plans" element={<Plans/>}/>
-        <Route path="/settings" element={<Settings/>}/>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Homepage />} />
+        <Route path="/plans" element={<Plans />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default MainRoute
+export default MainRoute;
--- server/Models/exercise.model.js
+++ server/Models/exercise.model.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
-const value = Math.random() * 105;
-
 const ExerciseSchema = mongoose.Schema({
-  Exercise: {
-    name: "Arm Ergometer",
-    amount: { val: 30, unit: "minutes" },
-    Energy: value.toFixed(1),
-  },
+  Exercise: { type: Object, required: true },
 });
 
 const ExerciseModel = mongoose.model("all_exercise", ExerciseSchema);
