92909a674d87b48b692172c0200e0684c99cbe23
Aakash Kushwaha <99542364+Aakash-kushwaha@users.noreply.github.com>
2022-08-30 19:51:02 +0530

    final

--- cronometer/src/routes/MainRoute.jsx
+++ cronometer/src/routes/MainRoute.jsx
@@ -10,43 +10,19 @@ import Login from "../pages/Login/Login";
 import Forget from "../pages/ForgetPassword/forget";
 import Profile from "../pages/Profile/Profile";
 import PrivateRoute from "./PrivateRoute";
-import { useSelector } from "react-redux";
+
 
 const MainRoute = () => {
-<<<<<<< HEAD
   let token = JSON.parse(localStorage.getItem("token"));
   console.log(token, "token");
   return (
     <div>
-      <Navbar />
-      <Routes>
-        <Route path="/about" element={<About />} />
-        <Route path="/" element={<Homepage />} />
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute>
-              <Addfood></Addfood>
-            </PrivateRoute>
-          }
-        ></Route>
-=======
-  let token = useSelector((state)=>state.authreducer.token)
-  React.useEffect(()=>{
-
-  },[token])
-  console.log("hello",token)
-  return (
-    <div>
-
-{/* <Navbar></Navbar> */}
     
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/home" element={<PrivateRoute><Addfood></Addfood></PrivateRoute>}></Route>
         <Route path="/diary" element={<Addfood></Addfood>}></Route>
->>>>>>> 6ccba0c0f4c89b3c6813217fa9406d2ebb91561e
         <Route path="/support" element={<Support />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/signup" element={<Signup />} />
